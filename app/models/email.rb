7758213c6927ec20e455d73c331df63ea651c30b
josh <>
2007-04-18 18:47:58 +0000

    friendlier from names

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,14 +1,14 @@
 class Email < ActionMailer::Base
   def invite_for_member(user, member)
     recipients user.login
-    from       "no-reply@dcp.usworker.coop"
+    from       "Data Commons Project <no-reply@dcp.usworker.coop>"
     subject    "You have been invited to a Data Commons member organization"
     body       :user => user, :member => member
   end
 
   def invite_for_entry(user, entry)
     recipients user.login
-    from       "no-reply@dcp.usworker.coop"
+    from       "Data Commons Project <no-reply@dcp.usworker.coop>"
     subject    "You have been invited to become a Data Commons entry editor"
     body       :user => user, :entry => entry
   end
