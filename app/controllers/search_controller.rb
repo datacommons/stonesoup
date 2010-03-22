2dd5bd59fcee4d7b1dc957a5c949c9e064a13f2b
bbradley@ronincollective.com <>
2010-03-22 13:02:38 -0500

    comment out member-related search clause

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,11 +14,11 @@ class SearchController < ApplicationController
     # if user is admin, don't append any conditions to search
     unless current_user && current_user.is_admin?
       # if user has a member org, allow user to search for member's entries
-      if current_user && current_user.member
-        @member_clause = "OR member_id:#{current_user.member.id}"
-      end
+#      if current_user && current_user.member
+#        @member_clause = "OR member_id:#{current_user.member.id}"
+#      end
       # restrict searches to public + (maybe) this member's entries
-      query += " +(public:true #{@member_clause})"
+#      query += " +(public:true #{@member_clause})"
     end
 
     if params[:q]
