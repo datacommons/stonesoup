3e7dfd92c54e4d742392906400832748ced9ae7a
bbradley@ronincollective.com <>
2010-10-20 12:20:35 -0500

    remove old member-related code

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,11 +1,4 @@
 class Email < ActionMailer::Base
-  def invite_for_member(user, member)
-    recipients user.login
-    from       "Data Commons Project <no-reply@dcp.usworker.coop>"
-    subject    "You have been invited to a Data Commons member organization"
-    body       :user => user, :member => member
-  end
-
   def invite_for_org(user, org)
     recipients user.login
     from       "Data Commons Project <no-reply@dcp.usworker.coop>"
