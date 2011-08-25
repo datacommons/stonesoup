daae0a98c83cdbe644f6a3cd60304dbf92fac030
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-08-25 18:03:58 +0200

    freshen organizations_people test

--- test/functional/organizations_people_controller_test.rb
+++ test/functional/organizations_people_controller_test.rb
@@ -14,30 +14,30 @@ class OrganizationsPeopleControllerTest < ActionController::TestCase
 
   test "should create organizations_person" do
     assert_difference('OrganizationsPerson.count') do
-      post :create, :organizations_person => { }
+      post :create, :organizations_person => { :person => people(:one), :organization => organizations(:one) }
     end
 
     assert_redirected_to organizations_person_path(assigns(:organizations_person))
   end
 
   test "should show organizations_person" do
-    get :show, :id => organizations_people(:one).id
+    get :show, :id => organizations_people(:hungry_coop_man).id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => organizations_people(:one).id
+    get :edit, :id => organizations_people(:hungry_coop_man).id
     assert_response :success
   end
 
   test "should update organizations_person" do
-    put :update, :id => organizations_people(:one).id, :organizations_person => { }
+    put :update, :id => organizations_people(:hungry_coop_man).id, :organizations_person => { }
     assert_redirected_to organizations_person_path(assigns(:organizations_person))
   end
 
   test "should destroy organizations_person" do
     assert_difference('OrganizationsPerson.count', -1) do
-      delete :destroy, :id => organizations_people(:one).id
+      delete :destroy, :id => organizations_people(:hungry_coop_man).id
     end
 
     assert_redirected_to organizations_people_path
