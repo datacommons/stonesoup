95ddffa083d31470884714aad6de39f31f41349f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-10-05 18:56:53 +0200

    add dc prototype

---
+++ app/sites/dc_site.rb
@@ -0,0 +1,18 @@
+class DcSite < Site
+
+  def site_searches
+    ['food','zip:20005*','*']
+  end
+
+  def aliases
+    ['dc.find.coop','testdc.find.coop']
+  end
+
+  def state_filter
+    ['District of Columbia', 'DC']
+  end
+  
+  def should_show_latest_people
+    false
+  end
+end
