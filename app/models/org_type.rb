445e15ec7427d2615bfab9f015cf43b3a84951c1
bbradley@ronincollective.com <>
2011-10-05 10:54:50 -0500

    reformatted: standardize on 2-space indent

--- app/models/org_type.rb
+++ app/models/org_type.rb
@@ -3,28 +3,30 @@ class OrgType < ActiveRecord::Base
   belongs_to :synonym_of, :class_name => 'OrgType', :foreign_key => 'effective_id'
   has_many :synonyms, :class_name => 'OrgType', :foreign_key => 'effective_id'
 
-   def get_organizations
-     Organization.find(:all, :conditions => ['x.org_type_id = ? OR org_types.effective_id = ?', self.id, self.id],
-       :joins => 'INNER JOIN org_types_organizations x ON x.organization_id = organizations.id
- INNER JOIN org_types ON x.org_type_id = org_types.id')
-   end
+  def get_organizations
+    Organization.find(:all, :conditions => ['x.org_type_id = ? OR org_types.effective_id = ?', self.id, self.id],
+      :joins => 'INNER JOIN org_types_organizations x ON x.organization_id = organizations.id' + \
+        ' INNER JOIN org_types ON x.org_type_id = org_types.id')
+  end
 
-   def root_term(original_root = self)
-     return self if synonym_of.nil?
-     if original_root == synonym_of  # prevent cirles
-       logger.error("Detected a synonym circle! starting with: #{original_root.inspect}")
-       return original_root 
-     end
-     return synonym_of.root_term(original_root)
-   end
+  def root_term(original_root = self)
+    return self if synonym_of.nil?
+    if original_root == synonym_of  # prevent cirles
+      logger.error("Detected a synonym circle! starting with: #{original_root.inspect}")
+      return original_root 
+    end
+    return synonym_of.root_term(original_root)
+  end
 
-   def OrgType.get_available
-     OrgType.find(:all, :conditions => 'custom = 0 AND (effective_id IS NULL OR effective_id = id)', :order => 'name')
-   end
-   def to_s
-     self.name
-   end
-   def <=>(other)
-     self.to_s <=> other.to_s
-   end
+  def OrgType.get_available
+    OrgType.find(:all, :conditions => 'custom = 0 AND (effective_id IS NULL OR effective_id = id)', :order => 'name')
+  end
+
+  def to_s
+    self.name
+  end
+
+  def <=>(other)
+    self.to_s <=> other.to_s
+  end
 end
