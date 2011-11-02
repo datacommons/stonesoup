65764449e28ab72b2aa0cfed40364b499617a2ce
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-11-02 20:56:59 +0100

    trim search examples

--- app/sites/default_site.rb
+++ app/sites/default_site.rb
@@ -1,7 +1,7 @@
 class DefaultSite < Site
 
   def site_searches
-    ['"Northeast Biodiesel"','Connecticut','cooperative','zip:02139','sector:consumer', 'sector:nonprofit AND state:massachusetts', 'tech*', 'sector:food AND (organic OR local)','sector:food -organic','Noemi Giszpenc']
+    ['Connecticut','cooperative','zip:02139','tech*', 'sector:food AND (organic OR local)','Noemi Giszpenc']
   end
 
   def aliases
