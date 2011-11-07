59ac562fddf9537d28fabcb0085fe152f19a70dd
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-11-07 15:52:28 +0100

    add civicrm-style countries

--- app/models/location.rb
+++ app/models/location.rb
@@ -19,6 +19,7 @@ class Location < ActiveRecord::Base
   Location::ADDRESS_FIELDS = ['address1', 'address2', 'city', 'state', 'zip', 'county', 'country']
   Location::MODIFIED_ADDRESS_FIELDS = ['address1', 'address2', 'city', 'state_zip', 'county', 'country']
   Location::STATES = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Marianas Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Virgin Islands', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+  Location::COUNTRY = ['United States', 'Canada']
 
   Location::STATE_SHORT = {
     'AL' => 'Alabama',
