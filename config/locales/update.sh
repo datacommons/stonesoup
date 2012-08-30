5da031f2740d5f4cfbb6c99281639fad994a9d31
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-08-30 22:15:05 +0200

    does translation need a commit to start?

---
+++ config/locales/update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Assume we edit:
+#   default.yml
+#   *.po
+# All other .yml files are auto-generated.
+# The *.pot file is auto-generated.
+
+if [ ! -e config/locales/default.yml ]; then
+    echo "Call from project root, please"
+    exit 1
+fi
+
+tmp="tmp_translate"
+
+base="$PWD"
+rm -rf $tmp
+
+mkdir -p $tmp || exit 1
+cd $tmp || exit 1
+ln -s ../config/locales locale || exit 1
+
+# from https://github.com/pejuko/i18n-translators-tools
+i18n-translate merge || exit 1
+
+i18n-translate convert -f po -t yml || exit 1
+cp locale/en.po locale/findcoop.pot || exit 1
+
+cd $base || exit 1
+rm -rf $tmp
+
