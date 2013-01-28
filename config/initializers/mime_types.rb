1278990c134eea68574df4fd350990eaf296c9b5
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-01-28 23:18:10 +0100

    workaround for another rails vulnerability CVE-2013-0333

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,4 @@ Mime::Type.register_alias "application/pdf", :pdf
 Mime::Type.register_alias "application/vnd.google-earth.kml+xml", :kml
 Mime::Type.register_alias "application/vnd.google-earth.kmz", :kmz
 ActionController::Base.param_parsers.delete(Mime::XML) 
+ActiveSupport::JSON.backend = "JSONGem" 
