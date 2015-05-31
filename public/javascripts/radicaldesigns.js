d112c4d684701af9ab3ab8b44c6f2bdfbf903a67
Mark Jenkins <mark@parit.ca>
2015-05-31 15:32:48 -0500

    map height now adapted to page height, changes by RadicalDesigns (http://radicaldesigns.org)

--- public/javascripts/radicaldesigns.js
+++ public/javascripts/radicaldesigns.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
  
-        jQuery("#map-text-box").show();
-        jQuery(".close-box").show();
+    jQuery("#map-text-box").show();
+    jQuery(".close-box").show();
         
  
     jQuery('.close-box').click(function(){
@@ -12,5 +12,14 @@ jQuery(document).ready(function(){
 	    jQuery("#map-text-box").toggle('slide');
 	    jQuery(".open-box").hide();
 	 });
- 
+	function map_height() {
+		var header = jQuery('#header').height();
+		var footer = jQuery('#footer').height();
+	    var height = jQuery(window).height();
+	    var map_height = (height) - (header) - (footer);
+	    map_height = parseInt(map_height) + 'px';
+	    jQuery("#big_front_map").css('height',map_height);
+	}
+    map_height();
+    jQuery(window).bind('resize', map_height);
 });
