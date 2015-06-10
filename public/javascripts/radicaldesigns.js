132bd93ba8787703245a680159276e64c5efbbf4
Mark Jenkins <mark@parit.ca>
2015-06-10 10:58:04 -0500

    design changes for search resoluts and organization listings by radical designs

--- public/javascripts/radicaldesigns.js
+++ public/javascripts/radicaldesigns.js
@@ -2,8 +2,7 @@ jQuery(document).ready(function(){
  
     jQuery("#map-text-box").show();
     jQuery(".close-box").show();
-        
- 
+
     jQuery('.close-box').click(function(){
     	jQuery("#map-text-box").toggle('slide');
     	jQuery(".open-box").show();
@@ -12,14 +11,25 @@ jQuery(document).ready(function(){
 	    jQuery("#map-text-box").toggle('slide');
 	    jQuery(".open-box").hide();
 	 });
+	 
 	function map_height() {
 		var header = jQuery('#header').height();
 		var footer = jQuery('#footer').height();
 	    var height = jQuery(window).height();
-	    var map_height = (height) - (header) - (footer);
-	    map_height = parseInt(map_height) + 'px';
+	    var map_height = parseInt((height) - (header) - (footer)) + 'px';
 	    jQuery("#big_front_map").css('height',map_height);
 	}
     map_height();
     jQuery(window).bind('resize', map_height);
+    
+	function sticky_footer() {
+		var content_height = parseInt(jQuery(window).height() - jQuery("#maincol").outerHeight() - jQuery("#header").height() - jQuery("#footer").height());
+		if(jQuery("#maincol").height() < content_height){
+			jQuery("#footer").css({
+				'margin-top': content_height
+			});
+		}
+	}
+	sticky_footer();
+	jQuery(window).bind('resize', sticky_footer);
 });
