e600ab67b135cfdc9ff38d7aad76b9f99dc03e96
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2017-09-30 18:56:33 +0000

    prototype new site

---
+++ app/sites/proto_site.rb
@@ -0,0 +1,56 @@
+class ProtoSite < Site
+
+  def site_searches
+    ['housing', 'Australia']
+  end
+
+  def aliases
+    ['proto.find.coop']
+  end
+
+  def dso_filter
+    nil
+  end
+
+  def state_filter
+    nil
+  end
+
+  def country_filter
+    nil
+  end
+
+  def should_show_latest_people
+    false
+  end
+
+  def layout
+    :sprint
+  end
+
+  def custom_css
+    name
+  end
+
+  def use_logo
+    false
+  end
+
+  def title
+    'Proto Find.coop'
+  end
+
+  def home
+    'http://proto.find.coop:4000'
+  end
+
+  def menu
+    [
+     {
+       :name => "Home",
+       :link => "/",
+       :id => "manitoba-button-find"
+     }
+    ]
+  end
+end
